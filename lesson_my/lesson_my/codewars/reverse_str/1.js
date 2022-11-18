9e888d69d72767d3fab6af1b519d7ba8e833b7cc
zjyxwl <116934360+zjyxwl@users.noreply.github.com>
2022-11-18 21:30:01 +0800

    Add files via upload

---
+++ lesson_my/lesson_my/codewars/reverse_str/1.js
@@ -0,0 +1,17 @@
+// 匿名函数
+// js 变量或常量的类型由值确定
+// 函数表达式
+const reverse = function(str) {
+    // let reverseStr = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     reverseStr += str[i]
+    // }
+    // return reverseStr
+    return str.split('').reverse().join('')
+}
+console.log(reverse('hello'));
+
+// 函数
+// function reverse() {
+
+// }
