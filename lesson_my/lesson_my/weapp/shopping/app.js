0d23133c38cdec1e13067509b3742301bec9bd70
zjyxwl <116934360+zjyxwl@users.noreply.github.com>
2022-12-13 16:59:49 +0800

    Add files via upload

--- lesson_my/lesson_my/weapp/shopping/app.js
+++ lesson_my/lesson_my/weapp/shopping/app.js
@@ -13,7 +13,22 @@ App({
       }
     })
   },
+  addToCart(obj) {
+    // 之前买过不？ 数量加 + 1
+    // 加入cartList 
+    //  es6 
+    const oldInfo = this.globalData.cartList
+      .find(item => item.iid === obj.iid );
+    console.log(oldInfo, '????');
+    if (oldInfo) {
+      oldInfo.count++;
+    } else {
+      obj.count = 1
+      obj.checked = true // 默认勾选
+      this.globalData.cartList.push(obj)
+    }
+  },
   globalData: {
-    userInfo: null
+    cartList: []
   }
 })
