ef9753310a30cf0c492a667d76d6db0e58e54a92
zjyxwl <3241158784@qq.com>
2023-02-22 14:08:03 +0800

    shopping

---
+++ lesson_my/lesson_my/vue3/vuex/shopping-cart/src/store/modules/products.js
@@ -0,0 +1,34 @@
+import shop from '../../api/shop'
+
+const state = {
+    all: []
+}
+
+const mutations = {
+    setProducts(state, products) {
+        console.log(products, 'mutations')
+        state.all = products
+    },
+    decrementProductInventory(state, { id }) {
+        const product = state.all.find(product => product.id === id )
+        product.inventory--
+    }
+}
+
+const actions = {
+    getAllProducts({ commit }) {
+        console.log('action,------')
+        shop.getProducts((products) => {
+            // vuex 制定了 数据的流程
+            // 修改数据到mutation
+            commit('setProducts', products)
+        })
+    }
+}
+
+export default {
+    namespaced: true,  // 模块的名字  store.products.state
+    state,
+    mutations,
+    actions
+}
