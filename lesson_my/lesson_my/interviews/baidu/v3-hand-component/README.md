4e5dd562d0258bffa032d9951feea8ad93121f05
zjyxwl <3241158784@qq.com>
2023-03-14 21:33:23 +0800

    v3

---
+++ lesson_my/lesson_my/interviews/baidu/v3-hand-component/README.md
@@ -0,0 +1,9 @@
+# 从零手写常用组件
+input   组件不是标签 是很多标签+ css + js 集合
+    通用的组件
+    - 基础需求 css input v-model
+    - 禁用 disable
+    - type password textarea
+    - 可清空
+    - 尺寸
+    
---
+++ lesson_my/lesson_my/interviews/baidu/v3-hand-component/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import Input from './components/Input.vue'
+
+const tel = ref('')
+// 组件通信角度
+// tel 跟 Input 组件双向绑定
+// 父子组件之间props 单向 props -> 
+// 子组件 emit input 标签change 传回来 修改tel
+setTimeout(() => {
+  tel.value = '13898978889'
+})
+</script>
+
+<template>
+  {{ tel }}
+<!-- <Input :model-value="tel" @update:model-value="tel=$event"/> -->
+<Input v-model="tel" :disabled="true" placeholder="请输入手机号"/>
+</template>
+
+<style scoped>
+
+</style>
